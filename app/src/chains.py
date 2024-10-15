51d2ca3a2b92afda31ad32970be20046252b9176
Shafaq Bibi <shafaqchohan123@gmail.com>
2024-10-15 09:00:31 +0500

    changes updated

--- app/src/chains.py
+++ app/src/chains.py
@@ -39,14 +39,14 @@ class Chain:
             {job_description}
 
             ### INSTRUCTION:
-            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
+            You are Shafaq, a business development executive at TechnoWolrd. TechnoWolrd is an AI & Software Consulting company dedicated to facilitating
             the seamless integration of business processes through automated tools. 
             Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
             process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
+            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of TechnoWolrd 
             in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-            Remember you are Mohan, BDE at AtliQ. 
+            Also add the most relevant ones from the following links to showcase TechnoWolrd's portfolio: {link_list}
+            Remember you are Shafaq, BDE at TechnoWolrd. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
 
